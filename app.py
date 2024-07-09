f3967a3ea8f56e16eff7a6503e92b0b6392e683a
Ronan McCormack <78881615+ronanmccormack-ca@users.noreply.github.com>
2024-07-09 10:29:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ app.layout = html.Div(id='parent', children=[
                 dcc.Dropdown(
                     id='date_dropdown',
                     options=[{'label': i, 'value': i} for i in data_process.get_dates()],
-                    value=max(data_process.get_dates()) if data_process.get_dates() else None
+                    value=max(data_process.get_dates())
                 ), width=10, lg=6, md=8, className="my-3 mx-auto"  # Adjusted for responsiveness
             )
         ])
